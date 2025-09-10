2c48b52d7aff9c619a9574b313ea4c7b60f4b77a
Mark Heramis <chumheramis@gmail.com>
2025-09-11 04:36:49 +0800

    update tmux configuration structure
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- install.sh
+++ install.sh
@@ -1,14 +1,8 @@
-# backup old config
-if [ -f ~/.tmux.conf ]; then
-  mv ~/.tmux.conf ~/.tmux.conf.bak
+if [ -f $HOME/.tmux.conf ]; then
+  mv $HOME/.tmux.conf $HOME/.tmux.conf.bak
 fi
-
-# get absolute path to current working directory
-CWD="$(pwd)"
-TMUX_CONF_SRC="$CWD/.tmux.conf"
-
-# symlink new config using absolute path
-ln -s -f "$TMUX_CONF_SRC" ~/.tmux.conf
-
-# copy local config using absolute path
-cp "$CWD/.tmux.conf.local" ~/.tmux.conf.local
+if [ -f $HOME/.tmux.conf.local ]; then
+  mv $HOME/.tmux.conf.local $HOME/.tmux.conf.local.bak
+fi
+ln -s $(pwd)/config/.tmux.conf $HOME/.tmux.conf
+cp $(pwd)/config/.tmux.conf.local $HOME/.tmux.conf.local
