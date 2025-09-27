fe6ea55180be727422107a52c542f72e8a8ed68f
Mark Heramis <chumheramis@gmail.com>
2025-09-27 09:52:10 +0800

    symlink .tmux.conf.local
    Signed-off-by: Mark Heramis <chumheramis@gmail.com>

--- install.sh
+++ install.sh
@@ -5,4 +5,4 @@ if [ -f $HOME/.tmux.conf.local ]; then
   mv $HOME/.tmux.conf.local $HOME/.tmux.conf.local.bak
 fi
 ln -s $(pwd)/config/.tmux.conf $HOME/.tmux.conf
-cp $(pwd)/config/.tmux.conf.local $HOME/.tmux.conf.local
+ln -s $(pwd).config/.tmux.conf.local $HOME/.tmux.conf.local
